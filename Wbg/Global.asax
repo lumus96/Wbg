521bde09fd701a740a08607385db74cb5e33ac68
Wbp <wbp@wbpnextes.com>
2016-12-09 15:55:23 +0200

    Add project files.

---
+++ Wbg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wbg.MvcApplication" Language="C#" %>
---
+++ Wbg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wbg.Startup))]
+namespace Wbg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
